42b3387972badbaea0eacea62727b640082edf89
¨GustaSacra¨ <gustavojosesacramento@gmail.com>
2025-08-21 16:41:59 -0300

    solucionando erro de conexao ao banco de dados

--- src/config/database.js
+++ src/config/database.js
@@ -1,16 +1,23 @@
-require("dotenv").config();
-const { Sequelize } = require("sequelize");
+require('dotenv').config({ path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env' });
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
+module.exports = {
+  // Adiciona a chave 'development' aqui
+  development: {
+    dialect: 'postgres',
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 5432,
-    dialect: "postgres",
-    logging: false,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    define: {
+      timestamps: true,
+      underscored: true,
+    },
+  },
+  // Você pode adicionar outros ambientes no futuro
+  test: {
+    // Configurações para o ambiente de teste
+  },
+  production: {
+    // Configurações para o ambiente de produção
   }
-);
-
-module.exports = sequelize;
+};
